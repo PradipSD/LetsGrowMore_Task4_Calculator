c9d83b26efba0ab82d44433f9f9fc51ff4b6e2e1
PradipSD <100369014+PradipSD@users.noreply.github.com>
2023-07-23 12:22:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+
+// LetsGrowMore
+//    Task4
+//     By 
+// Pradip Dound
+
+function clearScreen() {
+    document.getElementById("result").value = "";
+}
+ 
+
+function display(value) {
+    document.getElementById("result").value += value;
+}
+ 
+
+function calculate() {
+    var p = document.getElementById("result").value;
+    var q = eval(p);
+    document.getElementById("result").value = q;
+}
